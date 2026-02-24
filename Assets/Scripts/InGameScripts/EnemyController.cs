5fc3a7b268ef8c801113092b0cd353ffe42d15b8
HirumiIsna <hilmiisnaeni@student.ub.ac.id>
2026-02-24 07:36:31 +0700

    Implementing Attacking Range Enemy
    gak jadi habis kuliah jir
    
    - Berhubung system player health nya blom di implementasikan, cuma ada efek nembak doang gak akan ngurang nyawa playernya.
    - Health enemy range bisa diatur kayak enemy meleenya (ganti health di inspector).
    - Bullet ngikutin posisi terakhir player
    - Kecepatan bullet bisa diatur di prefab bulletnya.
    - Jarak musuh range ngedetek player bisa di atur di script EnemyController di bagian if distanceToPlayer.
    - Interval nembak bulletnya bisa di atur di script EnemyController bagian if _shootTimer.
    - Lifetime bullet bisa diatur di script BulletScript.
    
    KNOWN BUG!:
    - gak memengaruhi gameplay tapi agak mengganggu di consolenya, kalo misal ada tipe enemy yang Melee, nanti bakalan outputin error di consolenya karena bulletPrefabnya gak ada (Possible Solution : Misahin Scriptnya, atau mungkin ntar habis kuliah ku cari)
    
    Personal Note: jangan lupa ganti pake Unity Event System.

--- Assets/Scripts/InGameScripts/EnemyController.cs
+++ Assets/Scripts/InGameScripts/EnemyController.cs
@@ -7,9 +7,18 @@ public class EnemyController : MonoBehaviour
     private int _currentHealth;
     private SpriteRenderer _spriteRenderer;
     private Color _baseColor;
+    private GameObject _player;
+
+    // Range Enemy
+    private float _shootTimer;
+    public GameObject bulletPrefab;
+    public Transform bulletPos;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        _player = GameObject.FindGameObjectWithTag("Player");
+
         _spriteRenderer = GetComponent<SpriteRenderer>();
         _baseColor = _spriteRenderer.color;
 
@@ -19,7 +28,21 @@ public class EnemyController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+
+        float distanceToPlayer = Vector2.Distance(transform.position, _player.transform.position);
+        Debug.Log("Distance to Player: " + distanceToPlayer); // Buat ngecek jaraknya biar ngatur if nya gampang
+
+        if(distanceToPlayer < 7f)
+        {
+            _shootTimer += Time.deltaTime;
+            
+            if(_shootTimer > 2f)
+            {
+                _shootTimer = 0f;
+                Shoot();
+            }
+        }
+
     }
 
     public void TakeDamage(int damage)
@@ -40,5 +63,11 @@ public class EnemyController : MonoBehaviour
         _spriteRenderer.color = Color.red; 
         yield return new WaitForSeconds(0.1f); 
         _spriteRenderer.color = _baseColor; 
+    }
+
+    public void Shoot()
+    {
+        Debug.Log("Enemy Shoots!");
+        GameObject bullet =Instantiate(bulletPrefab, bulletPos.position, Quaternion.identity);
     }
 }
